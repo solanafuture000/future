efcb7ab66adb7af49702cec7f44165f9dfbadffc
solanafuture000 <aqibaqib7576@gmail.com>
2025-06-14 20:07:44 +0500

    Update upload.js

--- upload.js
+++ upload.js
@@ -2,22 +2,16 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-// Define the storage location and filename
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
+  destination: function (req, file, cb) {
     const dir = './uploads';
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
     cb(null, dir);
   },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const ext = path.extname(file.originalname);
-    cb(null, `${uniqueSuffix}${ext}`);
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
   }
 });
 
-// Create and export the multer upload instance
 const upload = multer({ storage });
 module.exports = upload;
