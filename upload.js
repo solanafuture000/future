5dab9da4d6e1e8a12432e014f30c6d25e568d406
solanafuture000 <aqibaqib7576@gmail.com>
2025-06-14 20:22:26 +0500

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,14 +1,14 @@
 const multer = require('multer');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: (req, file, cb) => {
     const dir = './uploads';
     if (!fs.existsSync(dir)) fs.mkdirSync(dir);
     cb(null, dir);
   },
-  filename: function (req, file, cb) {
+  filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
   }
 });
