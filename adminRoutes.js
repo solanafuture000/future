b7061e41747b017f45e3d6abd3f3e6ae8b6050e1
solanafuture000 <aqibaqib7576@gmail.com>
2025-07-02 16:13:26 +0500

    Update adminRoutes.js

--- adminRoutes.js
+++ adminRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const User = require('./User');
-const WithdrawRequest = require('../models/withdrawRequest');
+const WithdrawRequest = require('./models/withdrawRequest');
 
 // ✅ Authenticate Middleware
 const authenticate = (req, res, next) => {
